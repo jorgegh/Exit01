ba66d380386e8713ed439e7f7f0920b0710f37b6
Jesus <Android_27>
2017-01-30 23:20:50 +0100

    He insertado un toast inicial

--- app/src/main/java/com/example/dani/exit0/MainActivity.java
+++ app/src/main/java/com/example/dani/exit0/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.dani.exit0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button androidButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        androidButton = (Button)findViewById(R.id.button);
+        androidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "It is working!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
